a98ac952a003d1be6683d460e22221eaf17409dd
MrMako998 <matteo.makovec@gmail.com>
2020-03-28 20:57:44 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>    // <== direttive del preprocessore
+// i simboli < > dicono al preprocessore di cercare i file AL DI FUORI della cartella di progetto
+
+#define MY_HEADER_H_
+
+
+int main(int argc, char **argv) {
+
+	printf("Hello world!");
+
+	return 0;
+}
